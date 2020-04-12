f1b1fe059af4290ce3cb54c3664debbf443ea228
samsonl <lyndon.samson@gmail.com>
2020-04-12 15:38:20 +1000

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,5 +1,19 @@
-    libraries {
-        gradle2
+    dev{
+        long_name = "Development"
+        ec2{
+            ips = [ "1.2.3.4", "1.2.3.5" ]
+            credential_id = "ec2_ssh"
+        }
+        s3{
+            url = "https://s3.example.com"
+            path = "content/"
+            credential_id = "s3_bucket"
+        }
     }
+}
+  
+libraries {
+        gradle2
+}
 
    
